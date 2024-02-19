c447dc7c1e078c6fe13d2286f62b9fbb77f2673a
Meelad0708 <mazizollah@soldevelo.com>
2024-02-19 09:20:18 +0100

    added validationerrror

--- movies_py/mutation.py
+++ movies_py/mutation.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from .models import Genre, Movie
 from .queries import GenreType, MovieType
+from django.core.exceptions import ValidationError
 class CreateGenre(graphene.Mutation):
     class Arguments:
         name = graphene.String(required=True)
@@ -9,8 +10,14 @@ class CreateGenre(graphene.Mutation):
     genre = graphene.Field(GenreType)
 
     def mutate(self, info, name):
+        if not name:
+            raise ValidationError('Genre Name Cannot Be Empty')
+        if Genre.objects.filter(name=name).exists():
+            raise ValidationError('Genre Already Exists')
+
         genre = Genre.objects.create(name=name)
         return CreateGenre(genre=genre)
+
 
 class CreateMovie(graphene.Mutation):
     class Arguments:
@@ -23,6 +30,11 @@ class CreateMovie(graphene.Mutation):
     movie = graphene.Field(MovieType)
 
     def mutate(self, info, title, genre_id, rating, release, description):
+        if not title:
+            raise ValidationError('Movie Title Cannot Be Empty')
+        if Movie.objects.filter(title=title).exists():
+            raise ValidationError('Movie Already Exists')
+
         genre = Genre.objects.get(pk=genre_id)
         movie = Movie.objects.create(title=title, genre=genre, rating=rating, release=release, description=description)
         return CreateMovie(movie=movie)
